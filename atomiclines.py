fd46c6a0b1e21efe4497bfa8155b9e46d42cd6c0
nickalaskreynolds <nickalaskreynolds@gmail.com>
2018-01-25 14:07:14 -0600

    adding more lines

--- atomiclines.py
+++ atomiclines.py
@@ -1,50 +1,186 @@
-nir={"H2.0-0.S(0)":[28.221], "H2.0-0.S(1)":[17.035], "H2.0-0.S(2)":[12.279], \
-"H2.0-0.S(3)":[9.6649], "H2.0-0.S(4)":[8.0258], "H2.0-0.S(5)":[6.9091], \
-"H2.0-0.S(6)":[6.1088], "H2.0-0.S(7)":[5.5115], "H2.0-0.S(8)":[5.0529], \
-"H2.0-0.S(9)":[4.6947], "H2.0-0.S(10)":[4.4096], "H2.0-0.S(11)":[4.1810], \
-"H2.0-0.S(12)":[3.9947], "H2.0-0.S(13)":[3.8464], "H2.0-0.S(14)":[3.724], \
-"H2.0-0.S(15)":[3.625], "H2.0-0.S(16)":[3.547], "H2.0-0.S(17)":[3.485], \
-"H2.0-0.S(18)":[3.438], "H2.0-0.S(19)":[3.404], "H2.0-0.S(20)":[3.380], \
-"H2.0-0.S(21)":[3.369], "H2.0-0.S(22)":[3.366], "H2.0-0.S(23)":[3.372], \
-"H2.1-0.S(0)":[2.2235], "H2.1-0.S(1)":[2.1218], "H2.1-0.S(2)":[2.0338], \
-"H2.1-0.S(3)":[1.9576], "H2.1-0.S(4)":[1.8920], "H2.1-0.S(5)":[1.8358], \
-"H2.1-0.S(6)":[1.7880], "H2.1-0.S(7)":[1.7480], "H2.1-0.S(8)":[1.7147], \
-"H2.1-0.S(10)":[1.6665], "H2.1-0.S(11)":[1.6504], \
-"H2.1-0.Q(1)":[2.4066], "H2.1-0.Q(2)":[2.4134], "H2.1-0.Q(3)":[2.4237], \
-"H2.1-0.Q(4)":[2.4375], "H2.1-0.Q(5)":[2.4548], "H2.1-0.Q(6)":[2.4756], \
-"H2.1-0.Q(7)":[2.5001], "H2.1-0.O(2)":[2.6269], "H2.1-0.O(3)":[2.8025], \
-"H2.1-0.O(4)":[3.0039], "H2.1-0.O(5)":[3.2350], "H2.1-0.O(6)":[3.5007], \
-"H2.1-0.O(7)":[3.8075], "H2.1-0.O(8)":[4.1625], "H2.2-1.S(0)":[2.3556], \
-"H2.2-1.S(1)":[2.2477], "H2.2-1.S(2)":[2.1542], "H2.2-1.S(3)":[2.0735], \
-"H2.2-1.S(4)":[2.0041], "H2.2-1.S(5)":[1.9449], "H2.2-1.O(2)":[2.7862], \
-"H2.2-1.O(3)":[2.9741], "H2.2-1.O(4)":[3.1899], "H2.2-1.O(5)":[3.4379], \
-"H2.2-1.O(6)":[3.7236], "H2.2-1.O(7)":[4.0540], "H2.3-2.S(0)":[2.5014], \
-"H2.3-2.S(1)":[2.3864], "H2.3-2.S(2)":[2.2870], \
-"H2.3-2.S(4)":[2.1280], "H2.3-2.S(5)":[2.0656], "H2.3-2.S(6)":[2.0130], \
-"H2.3-2.S(7)":[1.9692], "H2.3-2.O(2)":[2.9620], "H2.3-2.O(3)":[3.1637], \
-"H2.3-2.O(4)":[3.3958], "H2.3-2.O(5)":[3.6630], "H2.3-2.O(6)":[3.9721], \
-"H2.2-0.S(1)":[1.1622], "H2.2-0.S(2)":[1.1382], \
-"H2.2-0.S(3)":[1.1175], "H2.2-0.S(4)":[1.0998], "H2.2-0.S(5)":[1.0851], \
-"H2.2-9.Q(1)":[1.2383], "H2.2-0.Q(2)":[1.2419], "H2.2-0.Q(3)":[1.2473], \
-"H2.2-0.Q(4)":[1.2545], "H2.2-0.Q(5)":[1.2636], "H2.2-0.O(2)":[1.2932], \
-"H2.2-0.O(3)":[1.3354], "H2.2-0.O(4)":[1.3817], "H2.2-0.O(5)":[1.4322],\
-'brg':[2.16611],\
-"pab":[1.28216],\
-"Fe2.a4d7-a6d9":[1.257],"Fe2.a4d7-a6d7":[1.321],"Fe2.a4d7-a4d9":[1.644],\
-"HeI":[1.0833], \
-"Na I":[0.818,1.1370,2.2040,2.206,2.208],\
-"FeH":[0.9895],\
-"K I":[1.1682,1.1765,1.2518,1.2425,1.5152],\
-"Mg I":[1.1820,1.4872,1.5020,1.5740,1.7095],\
-"Fe I":[1.1880,1.1970],\
-"Al I":[1.3115,1.67],\
-"Si I":[1.5875],\
-"Ca I":[1.9442,1.9755,2.2605,2.263,2.266],\
-"CO":[2.2925,2.3440,2.414,2.322,2.352,2.383],\
-"Fe II":[ 1.688,1.742], \
-"O I":[1.129],\
-"Ca II":[ 0.866,0.854,0.850]}
-# lines removed because conflicts
-'''
-"H2.4-3.S(3)":[2.3446], "H2.3-2.S(3)":[2.2014],"H2.1-0.S(9)":[1.6877],
-'''
+atomiclines={\
+    'nir':{\
+    "Al I":[1.3115,1.67,'micron'],\
+    "Ar III":[ 7137.8,7753.2,'angstrom'],\
+    'brg':[2.16611,'micron'],'Br5-4 ':[4.05226,'microns'],'Br6-4 ':[2.62587,'microns'],\
+    'Br8-4 ':[1.94509,'microns'],'Br9-4 ':[1.81791,'microns'],'Br10-4':[1.73669,'microns'],\
+    'Br11-4':[1.68111,'microns'],'Br12-4':[1.64117,'microns'],'Br13-4':[1.61137,'microns'],\
+    'Br14-4':[1.58849,'microns'],'Br15-4':[1.57050,'microns'],'Br16-4':[1.55607,'microns'],\
+    'Br17-4':[1.54431,'microns'],'Br18-4':[1.53460,'microns'],'Br19-4':[1.52647,'microns'],\
+    'Br20-4':[1.51960,'microns'],'Br21-4':[1.51374,'microns'],\
+    "Ca I":[1.9442,1.9755,2.2605,2.263,2.266,'micron'],\
+    #"Ca II":[3933.663,3968.468,8500.36, 8544.44,8664.52,'angstrom'],\
+    "CO":[2.2925,2.3440,2.414,2.322,2.352,2.383,'micron'],\
+    #"Fe I":[1.1880,1.1970,'micron'],\
+    "Fe II":[ 1.688,1.742,'micron'], \
+    "Fe2.a4d7-a6d9":[1.257,'micron'],"Fe2.a4d7-a6d7":[1.321,'micron'],"Fe2.a4d7-a4d9":[1.644,'micron'],\
+    "FeH":[0.9895,'micron'],\
+    #"H2.0-0.S(0)":[28.221,'micron'], "H2.0-0.S(1)":[17.035,'micron'], "H2.0-0.S(2)":[12.279,'micron'], \
+    #"H2.0-0.S(3)":[9.6649,'micron'], "H2.0-0.S(4)":[8.0258,'micron'], "H2.0-0.S(5)":[6.9091,'micron'], \
+    #"H2.0-0.S(6)":[6.1088,'micron'], "H2.0-0.S(7)":[5.5115,'micron'], "H2.0-0.S(8)":[5.0529,'micron'], \
+    #"H2.0-0.S(9)":[4.6947,'micron'], "H2.0-0.S(10)":[4.4096,'micron'], "H2.0-0.S(11)":[4.1810,'micron'], \
+    #"H2.0-0.S(12)":[3.9947,'micron'], "H2.0-0.S(13)":[3.8464,'micron'], "H2.0-0.S(14)":[3.724,'micron'], \
+    #"H2.0-0.S(15)":[3.625,'micron'], "H2.0-0.S(16)":[3.547,'micron'], "H2.0-0.S(17)":[3.485,'micron'], \
+    #"H2.0-0.S(18)":[3.438,'micron'], "H2.0-0.S(19)":[3.404,'micron'], "H2.0-0.S(20)":[3.380,'micron'], \
+    #"H2.0-0.S(21)":[3.369,'micron'], "H2.0-0.S(22)":[3.366,'micron'], "H2.0-0.S(23)":[3.372,'micron'], \
+    "H2.1-0.S(0)":[2.2235,'micron'], "H2.1-0.S(1)":[2.1218,'micron'], "H2.1-0.S(2)":[2.0338,'micron'], \
+    #"H2.1-0.S(3)":[1.9576,'micron'], "H2.1-0.S(4)":[1.8920,'micron'], "H2.1-0.S(5)":[1.8358,'micron'], \
+    #"H2.1-0.S(6)":[1.7880,'micron'], "H2.1-0.S(7)":[1.7480,'micron'], "H2.1-0.S(8)":[1.7147,'micron'], \
+    #"H2.1-0.S(10)":[1.6665,'micron'], "H2.1-0.S(11)":[1.6504,'micron'], \
+    "H2.1-0.Q(1)":[2.4066,'micron'], "H2.1-0.Q(2)":[2.4134,'micron'], "H2.1-0.Q(3)":[2.4237,'micron'], \
+    "H2.1-0.Q(4)":[2.4375,'micron'], "H2.1-0.Q(5)":[2.4548,'micron'], "H2.1-0.Q(6)":[2.4756,'micron'], \
+    #"H2.1-0.Q(7)":[2.5001,'micron'], "H2.1-0.O(2)":[2.6269,'micron'], "H2.1-0.O(3)":[2.8025,'micron'], \
+    #"H2.1-0.O(4)":[3.0039,'micron'], "H2.1-0.O(5)":[3.2350,'micron'], "H2.1-0.O(6)":[3.5007,'micron'], \
+    #"H2.1-0.O(7)":[3.8075,'micron'], "H2.1-0.O(8)":[4.1625,'micron'], "H2.2-1.S(0)":[2.3556,'micron'], \
+    #"H2.2-1.S(1)":[2.2477,'micron'], "H2.2-1.S(2)":[2.1542,'micron'], "H2.2-1.S(3)":[2.0735,'micron'], \
+    #"H2.2-1.S(4)":[2.0041,'micron'], "H2.2-1.S(5)":[1.9449,'micron'], "H2.2-1.O(2)":[2.7862,'micron'], \
+    #"H2.2-1.O(3)":[2.9741,'micron'], "H2.2-1.O(4)":[3.1899,'micron'], "H2.2-1.O(5)":[3.4379,'micron'], \
+    #"H2.2-1.O(6)":[3.7236,'micron'], "H2.2-1.O(7)":[4.0540,'micron'], "H2.3-2.S(0)":[2.5014,'micron'], \
+    #"H2.3-2.S(1)":[2.3864,'micron'], "H2.3-2.S(2)":[2.2870,'micron'], \
+    "H2.3-2.S(4)":[2.1280,'micron'], "H2.3-2.S(5)":[2.0656,'micron'], "H2.3-2.S(6)":[2.0130,'micron'], \
+    #"H2.3-2.S(7)":[1.9692,'micron'], "H2.3-2.O(2)":[2.9620,'micron'], "H2.3-2.O(3)":[3.1637,'micron'], \
+    #"H2.3-2.O(4)":[3.3958,'micron'], "H2.3-2.O(5)":[3.6630,'micron'], "H2.3-2.O(6)":[3.9721,'micron'], \
+    #"H2.2-0.S(1)":[1.1622,'micron'], "H2.2-0.S(2)":[1.1382,'micron'], \
+    #"H2.2-0.S(3)":[1.1175,'micron'], "H2.2-0.S(4)":[1.0998,'micron'], "H2.2-0.S(5)":[1.0851,'micron'], \
+    #"H2.2-9.Q(1)":[1.2383,'micron'], "H2.2-0.Q(2)":[1.2419,'micron'], "H2.2-0.Q(3)":[1.2473,'micron'], \
+    #"H2.2-0.Q(4)":[1.2545,'micron'], "H2.2-0.Q(5)":[1.2636,'micron'], "H2.2-0.O(2)":[1.2932,'micron'], \
+    #"H2.2-0.O(3)":[1.3354,'micron'], "H2.2-0.O(4)":[1.3817,'micron'], "H2.2-0.O(5)":[1.4322,'micron'],\
+    #"H2.4-3.S(3)":[2.3446,'micron'], "H2.3-2.S(3)":[2.2014,'micron'],"H2.1-0.S(9)":[1.6877,'micron'],
+    "H12":[3751.22,'angstrom'],"H11":[3771.70,'angstrom'],"H10":[3798.98,'angstrom'],"H9":[3836.48,'angstrom'],\
+    "H8":[3890.15,'angstrom'],"Hep":[3971.19,'angstrom'],"Hdel":[ 4102.92,'angstrom'],"Hgam":[ 4341.69,'angstrom'],\
+    "Hbeta":[ 4862.69,'angstrom'],"Halpha":[ 6564.61,'angstrom'],\
+    "He I":[0.388975,0.587730,0.6679996,1.0833,'micron'], \
+    'Humphreys10-6':[5.12865,4.67251,4.17080,4.02087,3.90755,3.81945,3.74940,3.69264,3.64593,\
+                     3.60697,3.57410,3.54610,3.52203,3.50116,3.48296,'microns'],
+    #"K I":[1.1682,1.1765,1.2518,1.2425,1.5152,'micron'],\
+    "O I":[1.129,0.630204,0.5578887,'micron'],\
+    "O II":[3726,3727.09,3729,3729.88,'micron'],\
+    "O III":[ 5008.24,4960.30,4364.44,'angstrom'],\
+    "N II":[ 6549.84, 6585.23, 5756.24,'angstrom'],\
+    "Mg":[ 5167.321,5172.684, 5183.604,'angstrom'],\
+    #"Mg I":[1.1820,1.4872,1.5020,1.5740,1.7095,'micron'],\
+    "Na I":[0.589158,0.589755,0.818,1.1370,2.2040,2.206,2.208,'micron'],\
+    "Ne II":[ 6585.23,6549.84,5756.24,'angstrom'],\
+    "Ne III":[0.386981,0.396853,'micron'],\
+    "S II":[ 6718.32,6732.71,'angstrom'],\
+    "S III":[ 6313.8,'angstrom'],\
+    "S III":[ 9071.1,9533.2,'angstrom'],\
+    "Si I":[1.5875,'micron'],\
+    "P11":[ 8865.217,'angstrom'],\
+    "P10":[ 9017.385,'angstrom'],\
+    "P9":[ 9231.547,'angstrom'],\
+    "P8":[ 9548.590,'angstrom'],\
+    "P7":[ 10052.128,'angstrom'],\
+    "paa":[1.875613,'micron'],"pab":[1.28216,'micron'],"pag":[1.0941091,'micron'],\
+    'Pa7-3 ':[1.00521,'microns'],'Pa8-3 ':[0.95486,'microns'],'Pa9-3 ':[0.92315,'microns'],
+    'Pa10-3':[0.90174,'microns'],
+    'Pfund':[4.65378,3.74056,3.29699,3.03920,2.87300,2.87300,2.75827,2.67513,2.61265,2.56433,\
+             2.52609,2.49525,2.46999,2.44900,2.43136,2.41639,2.40355,2.39248,2.38282,2.37438,\
+             2.36694,2.36035,2.35448,2.34924,2.34453,2.34028,2.33644,2.33296,2.32979,'microns']
+    },
+    'radio':{\
+    }
+}
+
+class lines(object):
+    '''
+    supported units all to anstroms so
+    micros = 10^4 since that many angstrongs
+    in microns
+    '''
+    def __init__(self):
+        self.units = {\
+                      'angstroms'  : 1,     \
+                      'micrometers': 10**4, \
+                      'millimeters': 10**7, \
+                      'centimeters': 10**8, \
+                      'meters'     : 10**10,\
+                      'kilometers' : 10**13 \
+                      }
+        self.types = ['nir']
+        self.fulllines = atomiclines
+
+    '''
+    This will resolve the units given and 
+    the type and configure the class accordingly
+    '''
+    def __call__(self,type='nir',bu='meters'):
+        self.bu = self.resolve_name(bu.lower())[1]
+        self.type = self.resolve_type(type.lower())
+        #print(self.type,self.convfac,self.bu)
+        return self.get_lines()
+
+    '''
+    Resolves the units and conversion factor
+    '''
+    def resolve_units(self,bu):
+        tmp = self.resolve_name(bu)
+        if tmp[0]:
+            return self.units[tmp[1]]
+        else:
+            raise RuntimeError('Unit: <{}> was not found in list of units: {}'.format(bu,self.get_units()))
+
+    '''
+    Will resolve the name of the 
+    unit from known types
+    '''
+    def resolve_name(self,bu):
+        resolve_units = {\
+                         'angstroms'  : ['ang','a','angs','angstrom'],      \
+                         'micrometers': ['microns','micron','mu','micrometres','micrometre','micrometer'], \
+                         'millimeters': ['mm','milli','millimetres','millimetre','millimeter'],   \
+                         'centimeters': ['cm','centi','centimetres','centimetre','centimeter'],   \
+                         'meters'     : ['m','metres','meter','metre'],\
+                         'kilometers' : ['km','kilo','kilometres','kilometre','kilometer'] \
+                         }
+
+        if bu not in self.get_units():
+            for i in resolve_units:
+                for k in resolve_units[i]:
+                    if bu == k:
+                        return True,i
+            return False,bu
+        else:
+            return True,bu
+
+    '''
+    Returns the types of line regions that have been defined
+    '''
+    def get_types(self):
+        return [x for x in self.types]
+
+    '''
+    Returns the units possible in the current setup
+    '''
+    def get_units(self):
+        return [x for x in self.units]
+
+    '''
+    Resolves Type
+    '''
+    def resolve_type(self,typel):
+        if typel not in self.get_types():
+            raise RuntimeError('Type: <{}> was not found in list of units: {}'.format(typel,self.get_types()))
+        else:
+            return typel
+
+    '''
+    returns conversion factor needed
+    '''
+    def conversion(self,init,fin):
+        return self.units[init]/self.units[fin]
+
+    '''
+    Returns the dictionary of all converted types
+    '''
+    def get_lines(self):
+        for i in atomiclines[self.type]:
+            for k,j in enumerate(atomiclines[self.type][i]):
+                if (k < (len(atomiclines[self.type][i]) -1)):
+                    initialun   = self.resolve_name(atomiclines[self.type][i][-1])[1]
+                    finalun     = self.bu
+                    initialconv = self.conversion(initialun,finalun)
+                    # print(initialun,finalun,initialconv)
+                    self.fulllines[self.type][i][k] = self.fulllines[self.type][i][k] * initialconv
+            del self.fulllines[self.type][i][-1]
+        return self.fulllines[self.type]
